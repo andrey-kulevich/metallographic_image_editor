8410f16b545d12e026c1b0abbfdb9b8fc47fd8d4
Tihomir_K <dronaldo12345@gmail.com>
2021-03-06 14:17:00 +0300

    implemented loading of images

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 var canvas = document.getElementById("cv")
 var ctx = canvas.getContext("2d")
+var files = []
+var images = []
+var img = new Image()
+
+$('#upload').click(() => {
+    $('#file-input').trigger('click')
+})
+
+$('#file-input').change((event) => {
+    files.push(event.target.files)
+    let reader = new FileReader()
+    reader.readAsDataURL(event.target.files[0])
+    reader.onload = (e) => {
+        if (e.target.readyState == FileReader.DONE) {
+            img.src = e.target.result
+            img.onload = () => ctx.drawImage(img, 0, 0)
+        }
+    }
+})
 
 
