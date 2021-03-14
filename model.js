cf824e34f9abfe858c861c0b708be081ad0b5061
Tihomir_K <dronaldo12345@gmail.com>
2021-03-14 19:06:54 +0300

    implemented drawing of fixed labels and changed style of tool bar

--- model.js
+++ model.js
@@ -1,5 +1,14 @@
 export class Action {
-    static type = Object.freeze({IMAGE_LOAD: 0, SCALE_LABEL: 1, LEFT_TOP_LABEL: 2, LEFT_BOTTOM_LABEL: 3, RANDOM_LABEL: 4, ARROW: 5})
+    
+    static type = Object.freeze({
+        IMAGE_LOAD: 0, 
+        SCALE_LABEL: 1, 
+        LEFT_TOP_LABEL: 2, 
+        LEFT_BOTTOM_LABEL: 3,
+        RIGHT_TOP_LABEL: 4, 
+        RANDOM_LABEL: 5, 
+        ARROW: 6})
+
     constructor(actionType, params) {
         this.actionType = actionType,
         this.params = params
@@ -9,7 +18,7 @@ export class Action {
 export class Editor {
 
     actionsList = []
-    scale = null
+    labels = []
     photos = []
     activePhoto = 0
 
@@ -17,7 +26,35 @@ export class Editor {
         this.canvasId = canvasId
         this.canvas = document.getElementById(canvasId)
         this.ctx = this.canvas.getContext("2d")
-        this.scale = new Scale(canvasId)
+        this.labels.push(new ScaleLabel(canvasId))
+        this.labels.push(new FixedLabel(canvasId, Action.type.LEFT_TOP_LABEL))
+        this.labels.push(new FixedLabel(canvasId, Action.type.LEFT_BOTTOM_LABEL))
+        this.labels.push(new FixedLabel(canvasId, Action.type.RIGHT_TOP_LABEL))
+    }
+
+    addAction(actionType, args) { this.actionsList.push(new Action(actionType, args)) }
+    
+    redraw() { 
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.actionsList.forEach(elem => {
+            switch(elem.actionType) {
+                case Action.type.IMAGE_LOAD:
+                    this.getActivePhoto().draw()
+                    break;
+                case Action.type.SCALE_LABEL:
+                    this.scaleLabel().draw(...elem.params)
+                    break;
+                case Action.type.LEFT_TOP_LABEL:
+                    this.fixedLabel(Action.type.LEFT_TOP_LABEL).draw(...elem.params)
+                    break;
+                case Action.type.LEFT_BOTTOM_LABEL:
+                    this.fixedLabel(Action.type.LEFT_BOTTOM_LABEL).draw(...elem.params)
+                    break;
+                case Action.type.RIGHT_TOP_LABEL:
+                    this.fixedLabel(Action.type.RIGHT_TOP_LABEL).draw(...elem.params)
+                    break;
+            }
+        }) 
     }
 
     loadPhotos(files) {
@@ -31,34 +68,29 @@ export class Editor {
     }
 
     setActivePhoto(index) { this.activePhoto = index }
+    
     getActivePhoto() { return this.photos[this.activePhoto] }
 
-    addAction(actionType, args) { this.actionsList.push(new Action(actionType, args)) }
-    
-    redraw() { 
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.actionsList.forEach(elem => {
-            switch(elem.actionType) {
-                case Action.type.IMAGE_LOAD:
-                    this.getActivePhoto().draw()
-                    break;
-                case Action.type.SCALE_LABEL:
-                    this.scale.draw(...elem.params)
-                    break;
-                default:
-                    break;
-            }
-        }) 
+    switchLabelsColor() { this.labels.forEach(elem => elem.switchColor()) }
+
+    scaleLabel() { return this.labels.find(elem => elem instanceof ScaleLabel) }
+
+    fixedLabel(type) { return this.labels.find(elem => elem.type === type) }
+
+    drawLabel(type, text) {
+        this.removeLabel(type)
+        this.addAction(type, [text])
+        this.redraw()
     }
 
     drawScaleLabel(type) {
-        this.removeScaleLabel()
+        this.removeLabel(Action.type.SCALE_LABEL)
         this.addAction(Action.type.SCALE_LABEL, type)
         this.redraw()
     }
 
-    removeScaleLabel() {
-        let index = this.actionsList.indexOf(this.actionsList.find(elem => elem.actionType === Action.type.SCALE_LABEL))
+    removeLabel(type) {
+        let index = this.actionsList.indexOf(this.actionsList.find(elem => elem.actionType === type))
         if (index > -1) this.actionsList.splice(index, 1)
     }
 
@@ -93,9 +125,24 @@ export class Photo {
     draw() { this.ctx.drawImage(this.img, 0, 0) }
 }
 
-export class Scale {
+export class Label {
 
     static Color = Object.freeze({WHITE: 0, BLACK: 1})
+    color = Label.Color.BLACK
+
+    constructor(canvasId) { 
+        this.canvasId = '#' + canvasId 
+        this.canvas = document.getElementById(canvasId)
+    }
+
+    switchColor() {
+        if (this.color === Label.Color.BLACK) this.color = Label.Color.WHITE
+        else this.color = Label.Color.BLACK
+    }
+}
+
+export class ScaleLabel extends Label {
+
     static Type = Object.freeze({
         RU: {
             x50: ['500 мкм', 183],
@@ -113,27 +160,22 @@ export class Scale {
         }
     })
 
-    color = Scale.Color.BLACK
-
-    constructor(canvasId) { 
-        this.canvasId = '#' + canvasId 
-        this.canvas = document.getElementById(canvasId)
-    }
+    constructor(canvasId) { super(canvasId) }
 
     draw(text, lineWidth) {
         const width = this.canvas.width
         const height = this.canvas.height
 
         $(this.canvasId).drawText({
-            fillStyle: this.color === Scale.Color.BLACK ? '#000' : '#fff',
+            fillStyle: this.color === Label.Color.BLACK ? '#000' : '#fff',
             strokeStyle: '#000',
-            strokeWidth: this.color === Scale.Color.BLACK ? 0 : 1,
+            strokeWidth: this.color === Label.Color.BLACK ? 0 : 1,
             x: width - 195, y: height - 50,
             fontSize: 75,
             fontFamily: 'Times New Roman',
             text: text
         }).drawRect({
-            fillStyle: this.color === Scale.Color.BLACK ? '#000' : '#fff',
+            fillStyle: this.color === Label.Color.BLACK ? '#000' : '#fff',
             strokeStyle: '#000',
             strokeWidth: 1,
             x: width - 200, y: height - 20,
@@ -141,4 +183,49 @@ export class Scale {
             height: 6
           });
     }
+}
+
+export class FixedLabel extends Label {
+
+    constructor(canvasId, actionType) { 
+        super(canvasId) 
+        this.type = actionType
+    }
+
+    draw(text) {
+        const width = this.canvas.width
+        const height = this.canvas.height
+        let posX = 0
+        let posY = 0
+
+        switch(this.type) {
+            case Action.type.LEFT_TOP_LABEL:
+                posX = 25
+                posY = 10
+                break;
+            case Action.type.LEFT_BOTTOM_LABEL:
+                posX = 25
+                posY = height - 70
+                break;
+            default:
+                posX = width - (40 * text.length + 5),
+                posY = 10
+                break;
+        }
+
+        $(this.canvasId).drawText({
+            fillStyle: this.color === Label.Color.BLACK ? '#000' : '#fff',
+            strokeStyle: '#000',
+            strokeWidth: this.color === Label.Color.BLACK ? 0 : 1,
+            x: posX, y: posY,
+            fontSize: 75,
+            fontFamily: 'Times New Roman',
+            text: text,
+            fromCenter: false
+        })
+    }
+}
+
+export class Arrow {
+
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { Editor, Scale } from './model.js'
+import { Action, Editor, ScaleLabel, Label } from './model.js'
 
 var canvas = document.getElementById('cv')
 var editor = new Editor('cv')
@@ -9,31 +9,59 @@ $('#file-input').change((event) => {
     $('.filename').text(editor.getActivePhoto().name)
 })
 
-var isEnglish = false
+var isScaleEnglish = false
 
 $('a[name=scaleLang]').click(() => { 
-    isEnglish = !isEnglish 
-    if (isEnglish) $('a[name=scaleLang]').css('background-color', '#edeff3')
+    isScaleEnglish = !isScaleEnglish 
+    if (isScaleEnglish) $('a[name=scaleLang]').css('background-color', '#edeff3')
     else $('a[name=scaleLang]').css('background-color', '#fff')
 })
 
-$('a[name=scaleColor]').click(() => { 
-    if (editor.scale.color === Scale.Color.BLACK) {
-        editor.scale.color = Scale.Color.WHITE
-        $('a[name=scaleColor]').css('background-color', '#edeff3')
+$('#labelColorButton').click(() => {
+    if ($('#labelColorButton').text() === 'Цвет: черный') {
+        $('#labelColorButton').removeClass('active')
+        $('#labelColorButton').text('Цвет: белый')
     } else {
-        editor.scale.color = Scale.Color.BLACK
-        $('a[name=scaleColor]').css('background-color', '#fff')
+        $('#labelColorButton').addClass('active')
+        $('#labelColorButton').text('Цвет: черный')
+    }
+    editor.switchLabelsColor()
+})
+
+$('a[name=x50scale]').click(() => { isScaleEnglish ? 
+    editor.drawScaleLabel(ScaleLabel.Type.EN.x50) : editor.drawScaleLabel(ScaleLabel.Type.RU.x50) })
+$('a[name=x100scale]').click(() => { isScaleEnglish ? 
+    editor.drawScaleLabel(ScaleLabel.Type.EN.x100) : editor.drawScaleLabel(ScaleLabel.Type.RU.x100) })
+$('a[name=x200scale]').click(() => { isScaleEnglish ? 
+    editor.drawScaleLabel(ScaleLabel.Type.EN.x200) : editor.drawScaleLabel(ScaleLabel.Type.RU.x200) })
+$('a[name=x500scale]').click(() => { isScaleEnglish ? 
+    editor.drawScaleLabel(ScaleLabel.Type.EN.x500) : editor.drawScaleLabel(ScaleLabel.Type.RU.x500) })
+$('a[name=x1000scale]').click(() => { isScaleEnglish ? 
+    editor.drawScaleLabel(ScaleLabel.Type.EN.x1000) : editor.drawScaleLabel(ScaleLabel.Type.RU.x1000) })
+
+$('a[name=leftTopLabel]').click(() => { $('#labelInput').attr('placeholder', 'Левая верхняя метка') })
+$('a[name=leftBottomLabel]').click(() => { $('#labelInput').attr('placeholder', 'Левая нижняя метка') })
+$('a[name=rightTopLabel]').click(() => { $('#labelInput').attr('placeholder', 'Правая верхняя метка') })
+$('a[name=randomLabel]').click(() => { $('#labelInput').attr('placeholder', 'Произвольная метка') })
+
+$('#labelButton').click(() => { 
+    const text = $('#labelInput').val()
+    switch($('#labelInput').attr('placeholder')) {
+        case 'Левая верхняя метка':
+            editor.drawLabel(Action.type.LEFT_TOP_LABEL, text) 
+            break;
+        case 'Левая нижняя метка':
+            editor.drawLabel(Action.type.LEFT_BOTTOM_LABEL, text) 
+            break;
+        case 'Правая верхняя метка':
+            editor.drawLabel(Action.type.RIGHT_TOP_LABEL, text) 
+            break;
+        default:
+            break;
     }
 })
 
-$('a[name=x50scale]').click(() => { isEnglish ? editor.drawScaleLabel(Scale.Type.EN.x50) : editor.drawScaleLabel(Scale.Type.RU.x50) })
-$('a[name=x100scale]').click(() => { isEnglish ? editor.drawScaleLabel(Scale.Type.EN.x100) : editor.drawScaleLabel(Scale.Type.RU.x100) })
-$('a[name=x200scale]').click(() => { isEnglish ? editor.drawScaleLabel(Scale.Type.EN.x200) : editor.drawScaleLabel(Scale.Type.RU.x200) })
-$('a[name=x500scale]').click(() => { isEnglish ? editor.drawScaleLabel(Scale.Type.EN.x500) : editor.drawScaleLabel(Scale.Type.RU.x500) })
-$('a[name=x1000scale]').click(() => { isEnglish ? editor.drawScaleLabel(Scale.Type.EN.x1000) : editor.drawScaleLabel(Scale.Type.RU.x1000) })
-
 $('#save').click((e) => {
-    e.target.setAttribute('download', 'img.jpg')
+    e.target.setAttribute('download', editor.getActivePhoto().name)
     e.target.setAttribute('href', canvas.toDataURL("image/jpg").replace("image/jpg", "image/octet-stream"))
 })
